afdb0fc8140915f2ef558c6b56903d1ce2143277
wangz10@acer <zichen.wang@mssm.edu>
2015-05-14 16:12:23 -0400

    produced gmt with predicted se-drug only

--- python/make_gmt.py
+++ python/make_gmt.py
@@ -9,6 +9,19 @@ HOME = expanduser("~")
 sys.path.append(HOME + '/Documents/bitbucket/maayanlab_utils')
 from fileIO import read_df, mysqlTable2dict, write_gmt
 
+## get known connections in SIDER
+d_umls_perts = {}
+conn = MySQLdb.connect(host='localhost',user='root', passwd='',db='drug_side_effects')
+cur = conn.cursor()
+query = """SELECT * FROM `%s`""" %'sider_connections'
+cur.execute(query)
+for row in cur:
+	pert_id, umls_id = row
+	umls_id = umls_id.strip()
+	if umls_id not in d_umls_perts:
+		d_umls_perts[umls_id] = [pert_id]
+	else: d_umls_perts[umls_id].append(pert_id)
+conn.close()
 
 d_pt_umls = mysqlTable2dict('sep', 'side_effects', 2, 1)
 # d_umls_pt = mysqlTable2dict('sep', 'side_effects', 1, 2)
@@ -19,13 +32,15 @@ d_pt_umls = mysqlTable2dict('sep', 'side_effects', 2, 1)
 # p_vals from the latest prediction
 ## get AUC for each side_effect
 
+
+
 aucs = np.loadtxt('D:/Zichen_Projects/drug_se_prediction/training_set_no_mfc/PTs/ExtraTrees100_RLogit_GO + MACCS_per-label_AUC_n794x1053.txt').mean(axis=1)
 print len(aucs)
 print np.percentile(aucs, 75)
 print aucs[aucs > 0.75].shape
 
 mat, pert_ids, se_names = read_df('D:/Zichen_Projects/drug_se_prediction/training_set_no_mfc/PTs/PTs_ETs100_proba_df_n20338x1053.txt')
-AUC_CUTOFF = 0.75
+AUC_CUTOFF = 0.7
 P_CUTOFF = 0.75 # probability cutoff
 print mat.shape
 
@@ -45,19 +60,28 @@ for se, p_vals in zip(se_names, mat.T):
 		ses_to_exclude.add(se)
 	else:
 		drugs = pert_ids[mask]
-		d_se_drugs[se] = drugs
+		umls = d_pt_umls[se]
+		if umls is not None:
+			if umls in d_umls_perts:
+				known_drugs = d_umls_perts[umls]
+				drugs = set(drugs) - set(known_drugs)
+			if len(drugs) != 0:
+				d_se_drugs[umls] = drugs
 
-write_gmt(d_se_drugs, HOME+'/Documents/N2C/Sets2Networks/ET100_GOtCS_AUC_%s_proba_%s_flip.gmt' %(AUC_CUTOFF, P_CUTOFF))
+print len(d_se_drugs)
+# for key, val in d_se_drugs.items():
+# 	print len(val)
+write_gmt(d_se_drugs, HOME+'/Documents/N2C/Sets2Networks/ET100_GOtCS_AUC_%s_proba_%s_prediction_only.gmt' %(AUC_CUTOFF, P_CUTOFF))
 
 
 ## make gmt file with drug as term and se as genes
-d_drug_ses = {}
-for pert_id, p_vals in zip(pert_ids, mat):
-	mask = p_vals > P_CUTOFF
-	SEs = [d_pt_umls[pt] for pt in se_names[mask] if d_pt_umls[pt] is not None and pt not in ses_to_exclude]
-	if len(SEs) != 0:
-		d_drug_ses[pert_id] = SEs
+# d_drug_ses = {}
+# for pert_id, p_vals in zip(pert_ids, mat):
+# 	mask = p_vals > P_CUTOFF
+# 	SEs = [d_pt_umls[pt] for pt in se_names[mask] if d_pt_umls[pt] is not None and pt not in ses_to_exclude]
+# 	if len(SEs) != 0:
+# 		d_drug_ses[pert_id] = SEs
 
-write_gmt(d_drug_ses, HOME+'/Documents/N2C/Sets2Networks/ET100_GOtCS_AUC_%s_proba_%s.gmt' %(AUC_CUTOFF, P_CUTOFF))
+# write_gmt(d_drug_ses, HOME+'/Documents/N2C/Sets2Networks/ET100_GOtCS_AUC_%s_proba_%s.gmt' %(AUC_CUTOFF, P_CUTOFF))
 
 
