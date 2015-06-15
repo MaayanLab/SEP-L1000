52d4038548cc28854c0e3a62ad71924f46360b85
Zichen Wang <wangzc921@gmail.com>
2015-06-15 15:11:58 -0400

    made directed json graphs for side effects; made prototypes for pack layout

--- python/network_layout.py
+++ python/network_layout.py
@@ -4,6 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import manifold
 import networkx as nx
+from networkx.readwrite import json_graph
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.cluster import AgglomerativeClustering
 
 from matplotlib import rcParams
 rcParams['pdf.fonttype'] = 42 ## Output Type 3 (Type3) or Type 42 (TrueType)
@@ -110,6 +113,76 @@ def make_network_json(layout_df, d_id_name, d_id_category, d_category_color, out
 	json.dump(objs, open(outfn, 'wb'))
 	return
 
+def make_directed_json_graph(gmt_fn, d_id_name, d_id_category, d_category_color, outfn=None):
+	# perform HC and make a directed graph and write to json
+	# for pack visualization
+	d_gmt = read_gmt(gmt_fn)
+	d_gmt_filt = {}
+	for term, genes in d_gmt.items():
+		if len(genes) >= 5:
+			d_gmt_filt[term] = genes
+	d_gmt = d_gmt_filt
+
+	print 'number of terms:', len(d_gmt)
+	umls_ids_kept = d_gmt.keys()
+	adj_matrix = jaccard_matrix(d_gmt)
+
+	hc = AgglomerativeClustering(n_clusters=10)
+	hc.fit(adj_matrix)
+
+	m = adj_matrix > 0.2
+	adj_matrix = adj_matrix * m.astype(int)
+	Gu = nx.from_numpy_matrix(adj_matrix) # undirected Graph, to get size
+
+	G = nx.DiGraph()
+	for i in range(adj_matrix.shape[0]):
+		cluster_label = hc.labels_[i]
+		umls_id = umls_ids_kept[i]
+		name = d_id_name[umls_id]
+		G.add_edge('root', cluster_label)
+		G.add_edge(cluster_label, umls_id)
+		G.node[umls_id]['size'] = Gu.degree(i)
+		G.node[umls_id]['label'] = name
+
+		category = d_id_category[umls_id]
+		color = d_category_color[category]
+		G.node[umls_id]['color'] = color
+	graph_data = json_graph.tree_data(G,root='root')
+	json.dump(graph_data, open(outfn, 'wb'))
+	return
+
+def make_directed_json_graph_soc(gmt_fn, d_id_name, d_id_category, d_category_color, outfn=None):
+	# make directed graph based on SOC - PT
+	d_gmt = read_gmt(gmt_fn)
+	d_gmt_filt = {}
+	for term, genes in d_gmt.items():
+		if len(genes) >= 5:
+			d_gmt_filt[term] = genes
+	d_gmt = d_gmt_filt
+
+	print 'number of terms:', len(d_gmt)
+	umls_ids_kept = d_gmt.keys()
+	adj_matrix = jaccard_matrix(d_gmt)
+	m = adj_matrix > 0.2
+	adj_matrix = adj_matrix * m.astype(int)
+	Gu = nx.from_numpy_matrix(adj_matrix) # undirected Graph, to get size
+	G = nx.DiGraph()
+	for i in range(len(umls_ids_kept)):
+		umls_id = umls_ids_kept[i]
+		name = d_id_name[umls_id]
+		category = d_id_category[umls_id]
+		color = d_category_color[category]
+
+		G.add_edge('root', category)
+		G.add_edge(category, umls_id)
+
+		G.node[umls_id]['size'] = Gu.degree(i)
+		G.node[umls_id]['label'] = name
+		G.node[umls_id]['color'] = color		
+	graph_data = json_graph.tree_data(G,root='root')
+	json.dump(graph_data, open(outfn, 'wb'))
+	return
+
 
 
 # DF_FN = HOME+'/Documents/Zichen_Projects/drug_se_prediction/ET100_GOtCS_AUC_0.75_proba_0.75_significance_scores_matrix.txt'
@@ -119,11 +192,12 @@ def make_network_json(layout_df, d_id_name, d_id_category, d_category_color, out
 PREDICTION_DF = HOME + '/Documents/Zichen_Projects/drug_se_prediction/PTs_RF1000_proba_df_n20338x1053.txt'
 
 ## for side effects
-# GMT_FN = HOME+'/Documents/Zichen_Projects/drug_se_prediction/RF1000_GOtCS_AUC_0.7_proba_0.6_prediction_only.gmt'
-# GML_FN = HOME+'/Documents/Zichen_Projects/drug_se_prediction/side_effect_network.gml'
+GMT_FN = HOME+'/Documents/Zichen_Projects/drug_se_prediction/RF1000_GOtCS_AUC_0.7_proba_0.6_prediction_only.gmt'
+# GMT_FN = HOME+'/Documents/Zichen_Projects/drug_se_prediction/RF100_GOtCS_AUC_0.7_proba_0.75.gmt' 
+GML_FN = HOME+'/Documents/Zichen_Projects/drug_se_prediction/side_effect_network.gml'
 ## for drugs
-GMT_FN = HOME+'/Documents/Zichen_Projects/drug_se_prediction/RF1000_GOtCS_AUC_0.7_proba_0.6_prediction_only_flipped.gmt'
-GML_FN = HOME+'/Documents/Zichen_Projects/drug_se_prediction/drug_network.gml'
+# GMT_FN = HOME+'/Documents/Zichen_Projects/drug_se_prediction/RF1000_GOtCS_AUC_0.7_proba_0.6_prediction_only_flipped.gmt'
+# GML_FN = HOME+'/Documents/Zichen_Projects/drug_se_prediction/drug_network.gml'
 
 CSV_FN = GML_FN.replace('.gml', '.csv')
 JSON_FN = CSV_FN.replace('.csv', '.json')
@@ -159,7 +233,14 @@ d_soc_color = dict(zip(set(d_umls_soc.values()), COLORS40))
 # write_gmt(d_gmtT, HOME+'/Documents/Zichen_Projects/drug_se_prediction/RF1000_GOtCS_AUC_0.7_proba_0.6_prediction_only_flipped.gmt')
 
 
-G = network_layout(GMT_FN, outfn=GML_FN)
+# G = network_layout(GMT_FN, outfn=GML_FN)
 
 # make_network_json(CSV_FN, d_umls_pt, d_umls_soc, d_soc_color, outfn=JSON_FN)
+
+# make directed graph for predicted SEs
+# make_directed_json_graph(GMT_FN, d_umls_pt, d_umls_soc, d_soc_color, 
+# 	outfn=HOME+'/Documents/Zichen_Projects/drug_se_prediction/side_effects_digraph_with_known.json')
+make_directed_json_graph(GMT_FN, d_umls_pt, d_umls_soc, d_soc_color, 
+	outfn=HOME+'/Documents/Zichen_Projects/drug_se_prediction/side_effects_digraph_soc.json')
+
 
